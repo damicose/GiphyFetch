daae6201fc67cb524db927bd2b8d08fc88f589eb
Sonny D <damicose14@gmail.com>
2019-08-13 16:05:09 -0500

    just kidding, fiddled with js so that clicking button clears previous gifs rather than stacking on top of previous

--- assets/java/app.js
+++ assets/java/app.js
@@ -8,7 +8,6 @@ function displayHeroInfo() {
     const queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         hero + "&api_key=b7NVjCWm7hvalev6T5ZVweXLvFUYFTAp&limit=10";
 
-    // Creating an fetch call for the specific hero button being clicked
     fetch(queryURL)
         .then(function (response) {
             return response.json()
@@ -16,35 +15,28 @@ function displayHeroInfo() {
         .then(function (responseJson) {
 
             const results = responseJson.data;
+            document.getElementById("gifs-appear-here").innerHTML = "";
 
-            // Looping over every result item
             for (let i = 0; i < results.length; i++) {
 
-                // Only taking action if the photo has an appropriate rating
                 if (results[i].rating === "g" || results[i].rating === "pg") {
-                    // Creating a div for the gif
                     const gifDiv = document.createElement("div");
 
-                    // Storing the result item's rating
                     const rating = results[i].rating;
 
-                    // Creating a paragraph tag with the result item's rating
                     const p = document.createElement("p");
                     p.innerHTML = "Rating: " + rating;
-                    // Creating an image tag
+
                     const heroImage = document.createElement("img");
                     heroImage.classList.add(".gif");
                     heroImage.setAttribute("data-state", "animate");
-
                     heroImage.setAttribute("src", results[i].images.fixed_height.url);
 
-                    // Appending the paragraph and heroImage we created to the "gifDiv" div we created
                     gifDiv.append(p);
                     gifDiv.append(heroImage);
 
-                    // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-                    document.getElementById("gifs-appear-here").prepend(gifDiv);
 
+                    document.getElementById("gifs-appear-here").prepend(gifDiv);
 
                     heroImage.addEventListener("click", function (event) {
 
@@ -78,20 +70,18 @@ function renderButtons() {
         document.getElementById("buttons-view").append(a);
 
         a.addEventListener("click", displayHeroInfo);
+        
     }
 }
 
 document.getElementById("add-hero").addEventListener("click", function (event) {
     event.preventDefault();
 
-    // This line grabs the input from the textbox
     let hero = document.getElementById("hero-input").value.trim();
 
-    // Adding the hero from the textbox to our array
     heroes.push(hero);
     console.log(heroes);
 
-    // Calling renderButtons which handles the processing of our hero array
     renderButtons();
 });
 
